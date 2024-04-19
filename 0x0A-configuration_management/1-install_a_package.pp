30677bb99f77943fdb00ca061b5bbe2b66caec17
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-19 19:39:17 +0100

    task 1 in puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#!/usr/bin/puppet
+
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask and its dependencies using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
