04688c4fd6703a784d04d4fe56b797650dc3d2c6
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-19 19:55:51 +0100

    task 1 in puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,30 @@
-# install flask -v 2.1.0
+# Define packages to be installed
+$python_version = '3.8.10'
+$pip_package = 'pip3'
+$werkzeug_version = '2.1.1'
+$flask_version = '2.1.0'
 
-exec { 'flask':
-  command => '/usr/bin/pip3 install flask==2.1.0 -v',
+# Ensure Python 3.8.10 is installed
+package { 'python3':
+  ensure => $python_version,
+}
+
+# Ensure pip3 is installed
+package { $pip_package:
+  ensure => installed,
+  require => Package['python3'],
+}
+
+# Ensure Werkzeug 2.1.1 is installed
+package { 'werkzeug':
+  ensure   => $werkzeug_version,
+  provider => 'pip3',
+  require  => Package[$pip_package],
+}
+
+# Ensure Flask 2.1.0 is installed
+package { 'flask':
+  ensure   => $flask_version,
+  provider => 'pip3',
+  require  => Package['werkzeug'],
 }
