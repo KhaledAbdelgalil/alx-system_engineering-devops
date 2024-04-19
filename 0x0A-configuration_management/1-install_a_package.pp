934b3e120a66afd11a724a0fba834ed32be3477b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-19 21:20:42 +0100

    task 1 modified in puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
-# Install Flask using pip3
-package { 'python3-pip':
-  ensure => installed,
+# install Flask (2.1.0)
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
