2c099e20d4d7d3607617a45d65df3bba8f44bd87
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-19 19:41:19 +0100

    task 1 in puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,23 @@
-#!/usr/bin/puppet
+# Ensure Python 3.8.10 is installed
+package { 'python3.8':
+  ensure => '3.8.10',
+}
 
-# Ensure python3-pip is installed
+# Install pip for Python 3.8
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask and its dependencies using pip3
+# Install Flask 2.1.0 using pip3
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask',
+  require => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1 using pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  creates => '/usr/local/lib/python3.8/dist-packages/werkzeug',
+  require => Package['python3-pip'],
 }
