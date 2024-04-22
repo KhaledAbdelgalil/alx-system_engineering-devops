097b9235d5267e7917d66398d377e1670dea1095
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-23 00:21:30 +0100

    task puppet in web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,8 @@
 # install a nginx server
 
-exec { 'install nginx':
-command => 'apt-get -y update; apt-get -y install nginx; ufw allow \'Nginx HTTP\'; echo \'Hello World!\' > /var/www/html/index.html; sed -i \'24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\' /etc/nginx/sites-available/default; service nginx start'
+exec { 'install_nginx':
+  command => '/bin/bash -c "apt-get -y update && apt-get -y install nginx && ufw allow \'Nginx HTTP\' && echo \'Hello World!\' > /var/www/html/index.html && sed -i \'24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\' /etc/nginx/sites-available/default && service nginx start"',
+  path    => ['/bin', '/usr/bin'],
+  creates => '/var/www/html/index.html',
+  require => Package['nginx'],
 }
