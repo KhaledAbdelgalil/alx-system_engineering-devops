6b659453ed709610958974d87297c024e52c7042
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-06-04 22:26:30 +0100

    task 2___2 advanced api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Function returns the titles of hot
+Function returns the titles of  hot
 posts listed for a given subreddit
 """
 import requests
-
 after = None
 
 
@@ -13,19 +12,18 @@ def recurse(subreddit, hot_list=[]):
     global after
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent': 'khaled'
+        'User-Agent': 'MyRedditAppApi/0.0.1 (by u/khaled)'
     }
     params = {"after": after}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return None
 
     results = response.json().get("data")
     after = results.get("after")
-    titles = [c.get("data").get("title") for c in results.get("children")]
-    hot_list.extend(titles)
-
     if after is not None:
-        return recurse(subreddit, hot_list)
+        recurse(subreddit, hot_list)
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
     return hot_list
