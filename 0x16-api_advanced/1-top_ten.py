130cafe0ad9882e29d51ce43b920c1bfe3beeaa4
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-06-04 21:02:38 +0100

    task 1 api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,8 @@ def top_ten(subreddit):
     """prints the titles of the first 10 hot
     posts listed for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
-        return 0
+        print("None")
+        return
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         'User-Agent': 'MyRedditAppApi/0.0.1 (by u/khaled)'
