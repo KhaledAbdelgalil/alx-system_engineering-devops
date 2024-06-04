e2a2088f4f3fcad01e97007539186eeb7e2792ca
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-06-04 21:33:05 +0100

    task 2_ advanced api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,8 +9,6 @@ after = None
 def recurse(subreddit, hot_list=[]):
     """Returns a list of titles of all hot posts on a given subreddit."""
     global after
-    if subreddit is None or type(subreddit) is not str:
-        return None
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         'User-Agent': 'MyRedditAppApi/0.0.1 (by u/khaled)'
