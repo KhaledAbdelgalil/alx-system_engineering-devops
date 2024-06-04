9cfbadaa34bd7a9babe5d0ba55090713c6687b1b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-06-04 20:09:24 +0100

    task 0_ api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent':
-        'Mozilla/5.0 \
-        (Windows NT 10.0; Win64; x64)\
-        AppleWebKit/537.36 \
-        (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
+        'User-Agent': (
+            'Mozilla/5.0 '
+            '(Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        )
+    }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
