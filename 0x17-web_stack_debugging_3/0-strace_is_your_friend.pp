ee44599bd6be051cb5780c4638dc88961d48363a
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-06-06 00:13:35 +0100

    task web debug 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # puppet file to fix the web debug3 problem
 
-exec { 'Fix web debug3 problem':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php'
+exec { 'Fix web-debug3 problem':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
